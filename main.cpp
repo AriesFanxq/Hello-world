5bf95ddf6f8d1a07aae613099ea171e5ac28a589
AriesFanxq <33773271+AriesFanxq@users.noreply.github.com>
2019-07-03 16:32:12 +0800

    add thrid.
    add two print.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,15 @@
 
 int main()
 {
+  std::cout << "thrid" << std::endl;
+  
   std::cout << "Hello World!" << std::endl;
   
+  std::cout << "thrid" << std::endl;
+  
   std::out << "This is a test file!" << std::endl;
+  
+  std::cout << "thrid" << std::endl;
   
   return true;
 }
